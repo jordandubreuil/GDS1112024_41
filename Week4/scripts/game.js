c8b62e2c7883f20fab6e496791b029abbd33cb8c
jordandubreuil <jdubreuil@neit.edu>
2024-10-22 17:24:05 -0400

    End of class week 4

--- Week4/scripts/game.js
+++ Week4/scripts/game.js
@@ -4,19 +4,38 @@ var ctx = canvas.getContext("2d");
 var fps = 1000/60;
 var timer = setInterval(game, fps);
 
-var x = 0;
+var x = 50;
 var y = canvas.height/2;
 var w = 100;
 var h = 100;
+var moveAmmountX = 10;
+var moveAmmountY = 10;
 
 function game(){
     //Clear the canvas
     ctx.clearRect(0,0,canvas.width,canvas.height);
     //Update the canvas
-    x+=1;
+    x += moveAmmountX;
+    y += moveAmmountY;
     //Draw our GameObjects
     ctx.fillStyle = "red";
-    ctx.fillRect(x,y,w,h);
+    //ctx.fillRect(x,y,w,h);
+    ctx.beginPath();
+    ctx.arc(x,y,50,0, 2* Math.PI);
+    ctx.fill();
+    //Stay in boundary of canvas
+    if(x > canvas.width - 50){     
+        moveAmmountX *= -1;
+    }
+    if(x < 50){
+        moveAmmountX *= -1;
+    }
+    if(y<50){
+        moveAmmountY *= -1;
+    }
+    if(y>canvas.height - 50){
+        moveAmmountY *= -1;
+    }
 
 }
 
---
+++ Week4/scripts/keyboard.js
@@ -0,0 +1,98 @@
+//canvas stuff
+var canvas = document.querySelector("canvas");
+var ctx = canvas.getContext('2d');
+var fps = 1000/60;
+var timer = setInterval(gameLoop, fps);
+
+
+//This is an event listener
+
+
+var w = false;
+var a = false;
+var s = false;
+var d = false;
+var up = false;
+var down = false;
+var left = false;
+var right = false;
+
+document.addEventListener('keydown',press);
+//the function that gets called when we press the keys
+function press(event){
+    //give us the keycode of every keypress
+    console.log(event.keyCode);
+    if(event.keyCode == 87){ w = true}
+    if(event.keyCode == 65){ a = true}
+    if(event.keyCode == 83){ s = true}
+    if(event.keyCode == 68){ d = true}
+    if(event.keyCode == 38){ up = true}
+    if(event.keyCode == 40){ down = true}
+    if(event.keyCode == 37){ left = true}
+    if(event.keyCode == 39){ right = true}
+}
+
+document.addEventListener('keyup',release);
+//the function that gets called when we press the keys
+function release(event){
+    //give us the keycode of every keypress
+    console.log(event.keyCode);
+    if(event.keyCode == 87){ w = false}
+    if(event.keyCode == 65){ a = false }
+    if(event.keyCode == 83){ s = false}
+    if(event.keyCode == 68){ d = false}
+    if(event.keyCode == 38){ up = false}
+    if(event.keyCode == 40){ down = false}
+    if(event.keyCode == 37){ left = false}
+    if(event.keyCode == 39){ right = false}
+}
+
+class GameObject{
+    constructor(){
+        this.x = canvas.width/2;
+        this.y = canvas.height/2;
+        this.radius = 50;
+        this.startAngle = 0;
+        this.endAngle = 2 * Math.PI;
+        this.vx = 0;
+        this.vy = 0;
+        this.friction = 0.9;
+        this.color = "hotpink"
+    }
+
+    drawObj(){
+        ctx.save();
+        ctx.fillStyle = this.color;
+        ctx.translate(this.x, this.y);
+        ctx.beginPath();
+        ctx.arc(0, 0, this.radius, this.startAngle, this.endAngle);
+        ctx.closePath();
+        ctx.fill();
+        ctx.restore();
+    }
+
+    move(){
+        this.vx = this.vx * this.friction;
+        this.vy = this.vy * this.friction;
+        this.x += this.vx;
+        this.y += this.vy;
+    }
+}
+
+//This creates and instance of our GameObject
+var player = new GameObject();
+var speed = 10;
+
+//This is our game loop
+function gameLoop(){
+    //Clears the canvas
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    //These are the input checks
+    if(a==true){player.vx = -speed}
+    if(d==true){player.vx = speed}
+    if(w==true){player.vy = -speed}
+    if(s==true){player.vy = speed}
+    player.move();
+    //Draws player
+    player.drawObj();
+}
